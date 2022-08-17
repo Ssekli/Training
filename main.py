82b3445020b0b4bf106d25641ceba24426542fef
Ssekli <111066892+Ssekli@users.noreply.github.com>
2022-08-17 16:46:35 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
-nom_de_la_personne = input("Quel est ton nom ?")
-age = input("Quel est ton age ?")
+#boucle et fonctions
+
+def demander_nom() :
+    nom = ""
+    while nom == "" :
+        nom = input("Quel est votre nom ?")
+    return nom
 
 
-try :
-    age_prochain = int(age) + 1
+def demander_age(nom) :
+    age_int = 0
+    while age_int == 0 :
+        age_str = input(nom + "Quel est votre age ?")
+        try:
+            age_int = int(age_str)
+        except ValueError:
+            print("erreur entrez un nombre")
+    return age_int
 
-    
-except : ValueError
-    print("entrez un nombre")
+def afficher_information_personne(nom, age) :
+    print()
+    print("vous vous appelez " + nom + ", vous avez " + str(age) + " ans")
+    print("L'an prochain vous aurez " + str(age+1) + " ans.")
+    print()
+    if age >= 18: 
+        print("Vous êtes majeur")
+    else:
+        print("Vous êtes mineur")
+        
+
+nom1 = demander_nom()
+nom2 = demander_nom()
+
+age1 = demander_age(nom1)
+age2 = demander_age(nom2)
+
+afficher_information_personne(nom1, age1)
+afficher_information_personne(nom2, age2)
 
 
-else :
-    print("je m'appelle" + nom_de_la_personne + "et j'ai" + age +"ans")
-    print("L'an prochain, vous aurez " + age_prochain + ".")

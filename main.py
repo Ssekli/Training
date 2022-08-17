136f93398253a2b8e76b665b35fc381210bb6035
Ssekli <111066892+Ssekli@users.noreply.github.com>
2022-08-17 17:04:12 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -22,11 +22,24 @@ def afficher_information_personne(nom, age) :
     print("vous vous appelez " + nom + ", vous avez " + str(age) + " ans")
     print("L'an prochain vous aurez " + str(age+1) + " ans.")
     print()
-    if age >= 18: 
+    if age > 60:
+        print("Vous êtes sénior")
+    elif age == 17: 
+        print("Vous êtes presque majeur")
+    elif age >= 12 and age < 18:
+        print("Vous êtes adolescent")
+    elif age == 18:
+        print("Tout juste majeur, félicitation !")
+    elif age > 18:
         print("Vous êtes majeur")
+    elif age == 1 or age == 2:
+        print("Vous êtes bébé")
+    elif age < 10:
+        print("Vous êtes un enfant")
+ 
     else:
-        print("Vous êtes mineur")
-        
+        print ("Vous êtes mineur")
+
 
 nom1 = demander_nom()
 nom2 = demander_nom()

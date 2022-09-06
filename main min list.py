430a72efaaf342fe2cbf8725889273742b190c20
Ssekli <111066892+Ssekli@users.noreply.github.com>
2022-09-06 12:03:49 +0200

    Add files via upload

--- main min list.py
+++ main min list.py
@@ -12,5 +12,20 @@ for i in range(len(driver_distance)) :
         min_distance = distance
         min_index = i
 
+
 print(f"Distance minimale : {min_distance} km")
 print(f"Index de la distance minimale : {min_index}")
+print(f"Le chauffeur s'appelle {driver_name[min_index]}")
+
+#driver_distance.sort() trie mais casse l'index
+ 
+
+#Meilleur facon car liste peu contenir liste ou tupple 
+name_distances = [("Bob", 1.5), ("Brian", 2.2), ("Paul", 0.4), ]
+
+min_distance_name = name_distances[0]
+for name_distance in name_distances :
+    if name_distance[1] < min_distance_name [1] :
+        min_distance_name = name_distance
+
+print(f"Distance minimale {min_distance_name[1]} Nom du pilote : {min_distance_name[0]}")
